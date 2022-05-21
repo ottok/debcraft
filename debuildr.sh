41dfa8ffa28ded89516c72797793e13e2951399e
Otto Kekäläinen <otto@kekalainen.net>
2022-05-21 11:56:40 -0700

    Update default Debian Sid equivalent now that 'kinetic' is out

--- debuildr.sh
+++ debuildr.sh
@@ -201,7 +201,7 @@ then
   # Current Launchpad Debian Sid equivalent
   if [ "$SERIES" = 'unstable' ] || [ "$SERIES" = 'sid' ] || [ "$SERIES" = 'UNRELEASED' ] || [ "$SERIES" = 'experimental' ]
   then
-    SERIES='jammy'
+    SERIES='kinetic'
   fi
 
   # Historical equivalents
