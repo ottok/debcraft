e7d73e29c18887b6449cdd0376c49170e24026d6
Otto Kekäläinen <otto@debian.org>
2024-05-24 19:58:23 -0700

    Refactor source code repository activation to support Ubuntu 24.04
    In Ubuntu 24.04 (Noble Numbat) the file /etc/apt/sources.list exists,
    but has only comments. Thus the `sed` invocation always failed. To avoid
    this, test first if newer format exists, and if it does, just update it
    and don't bother checking or updating old files.

--- src/container/enable-source-repositories.sh
+++ src/container/enable-source-repositories.sh
@@ -7,47 +7,47 @@ set -e
 set -o pipefail
 
 # show commands (debug)
-#set -x
+set -x
 
-echo "Enable deb-src repositories in container"
-
+# Documentation section hack to avoid too many comment line prefixes
 cat /dev/null <<EOF
+
 On older Debian/Ubuntu containers the sources typically look like this:
 
-$ cat /etc/apt/sources.list
-# deb http://snapshot.debian.org/archive/debian/20231120T000000Z bullseye main
-deb http://deb.debian.org/debian bullseye main
-# deb http://snapshot.debian.org/archive/debian-security/20231120T000000Z bullseye-security main
-deb http://deb.debian.org/debian-security bullseye-security main
-# deb http://snapshot.debian.org/archive/debian/20231120T000000Z bullseye-updates main
-deb http://deb.debian.org/debian bullseye-updates main
+  $ cat /etc/apt/sources.list
+  # deb http://snapshot.debian.org/archive/debian/20231120T000000Z bullseye main
+  deb http://deb.debian.org/debian bullseye main
+  # deb http://snapshot.debian.org/archive/debian-security/20231120T000000Z bullseye-security main
+  deb http://deb.debian.org/debian-security bullseye-security main
+  # deb http://snapshot.debian.org/archive/debian/20231120T000000Z bullseye-updates main
+  deb http://deb.debian.org/debian bullseye-updates main
 
 Alternatively they have commented out deb-src lines. Either way, the command
 below will work in creating a new additional file with all sources duplicated to
 have deb-src lines.
-EOF
-if [ -f /etc/apt/sources.list ]
-then
-  grep '^deb ' /etc/apt/sources.list | \
-    sed 's/^deb /deb-src /g' > /etc/apt/sources.list.d/sources-list-with-deb-src.list
-fi
 
-cat /dev/null <<EOF
 On newer Debian/Ubuntu containers the sources typically look like this:
 
-$ cat /etc/apt/sources.list.d/debian.sources
-Types: deb
-# http://snapshot.debian.org/archive/debian/20231120T000000Z
-URIs: http://deb.debian.org/debian
-Suites: sid
-Components: main
-Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+  $ cat /etc/apt/sources.list.d/debian.sources
+  Types: deb
+  # http://snapshot.debian.org/archive/debian/20231120T000000Z
+  URIs: http://deb.debian.org/debian
+  Suites: sid
+  Components: main
+  Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
 Adding a 'deb-src' on the 'Types' line is enough to activate source repos.
 EOF
-if [ -f /etc/apt/sources.list.d/debian.sources ]
+
+if [ -f /etc/apt/sources.list.d/debian.sources ] || [ -f /etc/apt/sources.list.d/ubuntu.sources ]
 then
-  sed 's/Types: deb/Types: deb deb-src/g' -i /etc/apt/sources.list.d/debian.sources
+  echo "Enable deb-src repositories in container (new format)"
+  sed 's/Types: deb/Types: deb deb-src/g' -i /etc/apt/sources.list.d/*.sources
+elif  [ -f /etc/apt/sources.list ]
+then
+  echo "Enable deb-src repositories in container (legacy format)"
+  grep '^deb ' /etc/apt/sources.list | \
+    sed 's/^deb /deb-src /g' > /etc/apt/sources.list.d/sources-list-with-deb-src.list
 fi
 
 apt-get update
