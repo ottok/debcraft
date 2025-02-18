3d45e6558b53d7f5c5243c72bdac38764eb17e1b
Otto Kekäläinen <otto@debian.org>
2025-02-17 21:33:03 -0800

    Apply patches with Quilt when dpkg-source is skipped (Closes: #11, #13)
    Normally dpkg-source applies the patches when creating a source package.
    Running a build on the directory that was used to create the source
    package will have the patches applied, or running a build on a directory
    that was extracted from a source package will have the patches applied.
    
    However, when we run binary-only builds, or autopkgtest directly on the
    Debian source repository, no patches are applied, and git needs to run
    explicitly.
    
    Make the Quilt run conditional on if a series file exists. If it does
    not, the package either has no packages, or the git branch has the
    patches already applied (e.g. when building a `gbp pq` branch directly).

--- src/container/debcraft-builder.sh
+++ src/container/debcraft-builder.sh
@@ -42,11 +42,13 @@ else
 
   # Normally dpkg-source applies the patches, but when running a binary-only
   # build directly in the Debian packaging git repository dpkg-source will be
-  # skipped and sources will remain unpatched
+  # skipped and sources will remain unpatched unless they are explicitly patched
+  # with Quilt
   if [ -f debian/patches/series ]
   then
-    log_warn "Debian patches are *not* applied as this is a binary-only build that omits invoking 'dpkg-source'."
-    log_warn "Run 'gbp pq import --force' or equivalent to apply patches before building."
+    log_info "Apply patches with Quilt"
+    export QUILT_PATCHES=debian/patches
+    quilt push -a
   fi
 fi
 
