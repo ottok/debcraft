85516a548a15a1799cac8646ed95b9ae11a0688e
Otto Kekäläinen <otto@debian.org>
2026-05-03 07:20:54 +0000

    Clear DEBCRAFT_PPA in test 10 to avoid interactive PPA prompt
    Test 10 verifies local release with `--release-to`, but if
    `DEBCRAFT_PPA` is present in the environment the release logic enters
    the PPA upload path and interactively prompts:
    
        Upload to PPA 'ppa:otto/ppa' release 'resolute' [Y|n]?
    
    This breaks non-interactive CI runs. Prefix the test command with
    `DEBCRAFT_PPA=''` so it only exercises the local release path.

--- tests/debcraft-tests.sh
+++ tests/debcraft-tests.sh
@@ -180,7 +180,7 @@ rm -rf "$tmp_release_dir"
 if [ -z "${CI:-}" ]
 then
   tmp_release_dir=$(mktemp --directory)
-  debcraft_test "release --with-binaries --release-to $tmp_release_dir"  "  gbp tag --verbose"
+  DEBCRAFT_PPA='' debcraft_test "release --with-binaries --release-to $tmp_release_dir"  "  gbp tag --verbose"
   if ! ls "$tmp_release_dir"/entr_*.deb &> /dev/null
   then
     echo "Packages not released to $tmp_release_dir"
